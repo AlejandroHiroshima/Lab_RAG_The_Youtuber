9173480027670a42a79ed01577a9a2d1427a168c
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-12-11 11:56:52 +0100

    started fixing frontend, to match the new backend

--- frontend/app.py
+++ frontend/app.py
@@ -5,24 +5,35 @@ from pathlib import Path
 APP_DIR = Path(__file__).parent
 IMAGE_PATH = APP_DIR / "image.png"
 
+def init_session_states():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+def display_chat_messages():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+def handle_user_input():
+    if prompt := st.chat_input("Enter your data related question"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        bot_response = st.session_state.bot.chat(prompt).get("bot")
+        response = f"The Youtuber: {bot_response}"
+
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
 def layout():
     st.image(str(IMAGE_PATH))
     st.markdown("#🙌Ask the almighty Youtuber🙌")
-    input = st.text_input(label= "Enter your data related question")
-
-    if st.button("Send") and input != "":
-        repsonse = requests.post('http://127.0.0.1:8000/rag/query', json={"prompt": input})
-
-        data = repsonse.json()
-
-        st.markdown("##Question:")
-        st.markdown(input)
-
-        st.markdown("## Answer:")
-        st.markdown(data["answer"])
-
-        st.markdown("## Source:")
-        st.markdown(data["filepath"])
+    display_chat_messages()
+    handle_user_input()
 
 if __name__ == '__main__':
+    init_session_states()
     layout()
